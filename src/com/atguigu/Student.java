7226ee2507d76006cbea507e4383d124e1e222c0
wukong6 <wukong@hainan.net>
2018-06-13 23:45:30 +0800

    添加

--- src/com/atguigu/Student.java
+++ src/com/atguigu/Student.java
@@ -6,6 +6,10 @@ public class Student {
 		System.out.println("常飞1");
 		System.out.println("常飞2");
 		System.out.println("常飞3");
+
 		System.out.println("yyyyyy");
+
+		System.out.println("xxxxxxx");
+		System.out.println("xxxxxxx");
 	}
 }
